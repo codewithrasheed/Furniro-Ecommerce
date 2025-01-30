2179b4e8b394aa5ac9f3091fd9df595b5268036a
Codewithrasheed <rasheedahmed4845@gmail.com>
2025-01-30 18:55:54 +0500

    layout-fix

--- src/app/(Pages)/about/page.tsx
+++ src/app/(Pages)/about/page.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import PageBanner from '@/components/PageBanner'
 import { Trophy, Users2, Clock, BadgeCheck } from 'lucide-react'
 import Image from 'next/image'
-import Link from 'next/link'
 
 const About = () => {
   return (
@@ -147,13 +146,6 @@ const About = () => {
           </div>
         </div>
       </section>
-=======
-        <div className="container mx-auto px-4 md:px-0 py-10 mt-10">
-          <h1 className='text-center text-4xl font-bold mb-10'>Template is not Provided for this Page</h1>
-          <Link href={'/blog'} className='flex justify-center'>
-          <button className='bg-[#B88E2F] text-white px-8 py-3 text-center hover:bg-[#B88E2F]'>Go to Blog Page</button>
-          </Link>
-        </div>
     </>
   )
 }
