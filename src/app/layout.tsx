87f99c28c451934f6af0d42621a1c12454551743
Codewithrasheed <rasheedahmed4845@gmail.com>
2025-02-03 22:13:19 +0500

    layout-fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Lato, Montserrat, Quicksand } from "next/font/google";
-import { Poppins } from "next/font/google";
+import { Lato } from "next/font/google";
 import LayoutWrapper from "@/components/LayoutWrapper";
 import { Toaster } from "@/components/ui/toaster";
 
@@ -10,16 +9,6 @@ const lato = Lato({
   subsets: ["latin"],
   weight: ["100", "300", "400", "700", "900"],
   variable: "--font-lato",
-});
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
-  variable: "--font-poppins",
-});
-const quicksand = Quicksand({
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700"],
-  variable: "--font-quicksand",
 });
 
 export const metadata: Metadata = {
